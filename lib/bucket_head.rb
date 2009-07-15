9d45c9c71522b5d64872d99288ca58b8c5da1606
John McGrath <john@wordie.org>
2009-07-14 23:13:37 -0400

    added response.content_type stuff to put

--- lib/bucket_head.rb
+++ lib/bucket_head.rb
@@ -1,21 +1,3 @@
-# Usage:
-
-# BucketHead.set_bucket_name('int-xml')
-# BucketHead.put('http://puffy.ec2.nytimes.com/api/phu/count.xml', 'phu_count.xml')
-
-# results in the contents of the specified URI being uploaded to:
-# http://int-xml.s3.amazonaws.com/phu_count.xml
-
-# by default buckets and anything put in them have 'public-read' access
-# you can set bucket permissions, if, prior to bucket creation, you call:
-# BucketHead.set_bucket_access( 'public-read|private|whatever')
-
-# you can set access on individual objects within buckets by passing permissions as an optional 3rd argument:
-# BucketHead.put('http://puffy.ec2.nytimes/api/phu/count.xml', 'phu_count.xml', 'private')
-
-# the default mime type is text/xml. if you want to set a different mime type, pass it in as an
-# optional third argument to the BucketHead.put:
-
 require 'rubygems'
 require 'net/ssh'
 require 'net/http'
@@ -29,12 +11,8 @@ rescue
   @@library='aws/s3'
 end
 
-# BucketHead.set_bucket_name 'int-tester'
-# BucketHead.set_bucket_access 'public-read'
-# BucketHead.put('http://wordie.org', 'test.html')
-
 # TO DO:
-# - deal with mime type issues aron mentioned
+# - deal with mime type
 
 class BucketHead
   
@@ -74,12 +52,15 @@ class BucketHead
   end
   
   # have this accept arrays and strings. if it's an array, loop and bucketize everything in it, with array index as key
-  # def self.put(uri, key, access='public-read', content_type='text/xml')
+  # valid args: uri, key, access, content_type
   def self.put(args={})
     raise "Required arguments are :uri and :key." unless (args.has_key?(:uri) && args.has_key?(:key))
-    access       = (args.has_key?(:access)) ? args[:access] : 'public-read'
-    content_type = (args.has_key?(:content_type)) ? args[:content_type] : 'text/xml'
-    response     = Net::HTTP.get_response( URI.parse(args[:uri]) )
+    response      = Net::HTTP.get_response( URI.parse(args[:uri]) )
+    access        = (args.has_key?(:access)) ? args[:access] : 'public-read'
+    @content_type = (args.has_key?(:content_type)) ? args[:content_type] : response.content_type
+    
+    # these both use response.body, make sure it's an IO object.
+    # can this get the content_type from the response?
     case response.code
       when "200"
         if self.library == 'right_aws'
@@ -88,7 +69,7 @@ class BucketHead
           AWS::S3::S3Object.store( args[:key],
                                    response.body,
                                    BucketHead.bucket.name,
-                                   { :content_type => content_type, :access => access } )
+                                   { :content_type => @content_type, :access => access } )
         end
       else
         raise "Could not retreive file to bucketize."
